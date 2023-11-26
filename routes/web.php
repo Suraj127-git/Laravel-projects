281c232456c5830e4119f741dec34181bcb242e8
@shetty44444 <shetty44444@gmail.com>
2023-11-27 05:02:43 +0530

    added new changes

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // return redirect()->route('tasks.index');
-    return 'Welcome';
+    return redirect()->route('tasks.index');
+    // return 'Welcome';
 });
 
 Route::get('/tasks', function () {
