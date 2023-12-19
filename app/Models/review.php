150a096dac73e30b9d481ad196d555cd4ad1f171
Suraj Shetty <shetty44444@gmail.com>
2023-12-19 13:20:34 +0530

    Book Review Project started

--- app/Models/review.php
+++ app/Models/review.php
@@ -4,15 +4,14 @@ namespace App\Models;
 
 // Rest of the code for the Book class
 
-
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Book extends Model
+class review extends Model
 {
     use HasFactory;
 
-    public function review() {
-        return $this->hasMany(Review::class);
+    public function book() {
+        return $this->belongsTo(Book::class);
     }
 }
--- database/migrations/2023_12_19_054721_create_reviews_table.php
+++ database/migrations/2023_12_19_054721_create_reviews_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('author');
+            $table->unsignedBigInteger('book_id');
+            $table->text('reviews');
+            $table->unsignedTinyInteger('rating');
             $table->timestamps();
+
+            $table->foreign('book_id')->references('id')->on('books')->onDelete('cascade');
         });
     }
 
@@ -28,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('reviews');
     }
 };
