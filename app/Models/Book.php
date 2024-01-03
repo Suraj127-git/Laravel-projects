324c2b94cd451e0ec84ce1ce3be78b829742e87c
Suraj Shetty <shetty44444@gmail.com>
2024-01-03 14:34:23 +0530

    added db logic

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,7 @@
 
 namespace App\Models;
 
-// Rest of the code for the Book class
-
-
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,5 +12,34 @@ class Book extends Model
 
     public function review() {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder {
+        return $query->withCount(['reviews' => fn (Builder $q) => $this->DateRangeFilter($q, $from, $to)])  
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder {
+        return $query->withAvg(['reviews' => fn (Builder $q) => $this->DateRangeFilter($q, $from, $to)], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function DateRangeFilter(Builder $query, $from = null, $to = null) {
+        if($from &&!$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif(!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif($from && $to) {
+            $query->WhereBetween('created_at', $from, $to);
+        }
+        return $query;
     }
 }
